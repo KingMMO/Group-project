9380507907c5e412f5027bcfbebe3da04979b830
tsentuam <159571871+tsentuam@users.noreply.github.com>
2024-02-09 19:18:24 +0100

    Add files via upload

---
+++ clock_1.py
@@ -0,0 +1,7 @@
+from tkinter import * # this is were we import the library responsible for the functioning of the application
+clk = Tk() # this is the creation and start of the program
+clk.title("the clock") # this is the name of the application
+clk.geometry("1350x700+0+0") # width, height, x-axis, y-axis, we have kept 0 because we will start form left top corner
+clk.config(bg = "#ab245f") # you can give ny color you want
+
+clk.mainloop() # this is the end of the application
